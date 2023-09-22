78519a93d0a65202ff218e45e209d1b494376361
pjh5474 <warwarsn@gmail.com>
2023-09-22 12:28:31 +0900

    createAccount E2E Test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,6 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
+import { MailerService } from '@nestjs-modules/mailer';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -10,6 +13,15 @@ describe('UserModule (e2e)', () => {
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [
+        {
+          provide: MailerService,
+          useValue: {
+            sendMail: jest.fn(),
+            createTransport: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     app = module.createNestApplication();
@@ -31,9 +43,66 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
+  describe('createAccount', () => {
+    const EMAIL = 'nico@niconico.com';
+    it('should create account', () => {
+      jest
+        .spyOn(MailerService.prototype, 'sendMail')
+        .mockImplementation(async () => {
+          console.log('sendMail');
+        });
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password: "1234",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input: {
+            email:"${EMAIL}",
+            password: "1234",
+            role: Owner
+          }) {
+            ok
+            error
+          }
+        }
+      `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
