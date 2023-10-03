fa3d40160bdedce98d90b0a7e3fab7ddb83e88c0
pjh5474 <warwarsn@gmail.com>
2023-10-04 01:23:22 +0900

    createPayment & getPayments

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
+import { GetPaymentOutput } from './dtos/get-payments.dto';
+import { Payment } from './entities/payment.entity';
+
+@Injectable()
+export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly payments: Repository<Payment>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+  ) {}
+
+  async createPayment(
+    owner: User,
+    { transactionId, restaurantId }: CreatePaymentInput,
+  ): Promise<CreatePaymentOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: {
+          id: restaurantId,
+        },
+      });
+
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found',
+        };
+      }
+
+      if (restaurant.ownerId !== owner.id) {
+        return {
+          ok: false,
+          error: "You are not allowed to pay for others' restaurants",
+        };
+      }
+
+      await this.payments.save(
+        this.payments.create({
+          transactionId,
+          user: owner,
+          restaurant,
+        }),
+      );
+
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not create payment',
+      };
+    }
+  }
+
+  async getPayments(user: User): Promise<GetPaymentOutput> {
+    try {
+      const payments = await this.payments.find({
+        where: {
+          user: {
+            id: user.id,
+          },
+        },
+      });
+
+      return {
+        ok: true,
+        payments,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not get payments',
+      };
+    }
+  }
+}
